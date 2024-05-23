bd1db8ab4667bd81e1b567173f984a0ef4d90756
brennen <161400377+brennenxyz@users.noreply.github.com>
2024-05-22 22:58:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 import { getFormattedChat } from "../ClientsideChat/index"
 import PogObject from "../PogData"
-import DiscordClient from "../discord";
 
 let rewarping = false;
 
 const mc = Client.getMinecraft()
-const data = new PogObject("Rewarper", { rewarpPos: [], type: "netherwart:1", token: "" })
+const data = new PogObject("Rewarper", { rewarpPos: [], type: "netherwart:1" })
 const fhPests = Java.type("com.jelly.farmhelperv2.feature.impl.PestsDestroyer")
 const shift = new KeyBind(mc.field_71474_y.field_74311_E);
-const client = new DiscordClient({ intents: 3276799 });
-const display = Java.type("org.lwjgl.opengl.Display")
-const displayMode = Java.type("org.lwjgl.opengl.DisplayMode")
+
 const rewarp = new Thread(() => {
     Thread.sleep(1500)
     ChatLib.command("/ez-stopscript", true)
@@ -64,32 +61,7 @@ register("serverDisconnect", () => {
     Client.connect("hypixel.net")
 })
 
-register("command", (arg1) => {
-    data.token = arg1
-    data.save()
-    ChatLib.chat("Rewarper => Token has been set, reloading ChatTriggers...")
-    ChatTriggers.reloadCT()
-}).setName("settoken")
-
-client.on("ready", (user) => {
-    ChatLib.chat(`Rewarper => Discord Bot Logged in as ${user.username}!`);
-});
-
-client.on("message", (message) => {
-    if (message.content === '!start') {
-        ChatLib.command("ez-startscript netherwart:1", true);
-        message.reply("Started Taunahi Script: " + data.type + ".");
-    }
-
-    if (message.content === '!stop' || message.content === '!exit') {
-        ChatLib.command("ez-stopscript", true);
-        message.reply("Stopped Taunahi Script.");
-    }
-});
-
 register("command", () => {
     display.setDisplayMode(new displayMode(50, 50))
     display.setResizable(true);
 }).setName("smallwindow")
-
-client.login(data.token);
