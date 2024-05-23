4ccd6606d15539dbc367a2051b4e2a2c8b528a45
brennen <161400377+brennenxyz@users.noreply.github.com>
2024-05-22 22:54:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 import { getFormattedChat } from "../ClientsideChat/index"
 import PogObject from "../PogData"
-
+import DiscordClient from "../discord";
 
 let rewarping = false;
 
-const data = new PogObject("Rewarper", { rewarpPos: [], type: "netherwart:1" })
+const mc = Client.getMinecraft()
+const data = new PogObject("Rewarper", { rewarpPos: [], type: "netherwart:1", token: "" })
 const fhPests = Java.type("com.jelly.farmhelperv2.feature.impl.PestsDestroyer")
-
+const shift = new KeyBind(mc.field_71474_y.field_74311_E);
+const client = new DiscordClient({ intents: 3276799 });
+const display = Java.type("org.lwjgl.opengl.Display")
+const displayMode = Java.type("org.lwjgl.opengl.DisplayMode")
 const rewarp = new Thread(() => {
+    Thread.sleep(1500)
     ChatLib.command("/ez-stopscript", true)
     ChatLib.command("warp garden")
     Thread.sleep(1500)
-    if(!fhPests.getInstance().canEnableMacro(true)) {
-        ChatLib.command("ez-startscript " + data.type, true)
-    } else {
-        fhPests.getInstance().start()
-    }
-
+    fhPests.getInstance().start()
     rewarping = false;
 })
 
@@ -34,7 +34,7 @@ register("step", () => {
     let formattedChat = getFormattedChat(2);
 
     formattedChat.forEach((messageContent) => {
-        if (messageContent.includes("§r[Pests Destroyer] Stopping!")) {
+        if (messageContent.includes("[Pests Destroyer] Stopping")) {
             fhPests.getInstance().stop()
             ChatLib.chat("[Rewarper] Pests Destroyer Stopped...")
             
@@ -42,8 +42,12 @@ register("step", () => {
             ChatLib.chat("[Rewarper] Rewarping...")
 
             setTimeout(() => {
+                shift.setState(true)
                 ChatLib.command("ez-startscript " + data.type, true)
                 ChatLib.chat("[Rewarper] Starting Taunahi Script...")
+                setTimeout(() => {
+                    shift.setState(false)
+                }, 150);
             }, 1500);
         }
     })
@@ -51,6 +55,41 @@ register("step", () => {
     if(Math.floor(Player.getX()) == data.rewarpPos[0] && Math.floor(Player.getY()) == data.rewarpPos[1] && Math.floor(Player.getZ()) == data.rewarpPos[2]) {
         if(rewarping) return;
         rewarping = true;
+        ChatLib.command("/ez-stopscript", true)
         rewarp.start()
     }
 })
+
+register("serverDisconnect", () => {
+    Client.connect("hypixel.net")
+})
+
+register("command", (arg1) => {
+    data.token = arg1
+    data.save()
+    ChatLib.chat("Rewarper => Token has been set, reloading ChatTriggers...")
+    ChatTriggers.reloadCT()
+}).setName("settoken")
+
+client.on("ready", (user) => {
+    ChatLib.chat(`Rewarper => Discord Bot Logged in as ${user.username}!`);
+});
+
+client.on("message", (message) => {
+    if (message.content === '!start') {
+        ChatLib.command("ez-startscript netherwart:1", true);
+        message.reply("Started Taunahi Script: " + data.type + ".");
+    }
+
+    if (message.content === '!stop' || message.content === '!exit') {
+        ChatLib.command("ez-stopscript", true);
+        message.reply("Stopped Taunahi Script.");
+    }
+});
+
+register("command", () => {
+    display.setDisplayMode(new displayMode(50, 50))
+    display.setResizable(true);
+}).setName("smallwindow")
+
+client.login(data.token);
